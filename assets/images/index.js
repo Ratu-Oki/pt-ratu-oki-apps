f9297992e20cb16a04551b718392ac5b54698878
ryan88-cpu <ryansupit88@gmail.com>
2026-01-19 22:11:28 +0800

    Add image assets and update App component header text

---
+++ assets/images/index.js
@@ -0,0 +1,13 @@
+import IMGadmin from './admin-img.png';
+import IMGemail from './email-img.png';
+import IMGkonsumen from './konsumen-img.png';
+import IMGpassword from './password-img.png';
+import IMGcontainer from './container-img.png';
+
+export default {
+    IMGadmin,
+    IMGemail, 
+    IMGkonsumen,
+    IMGpassword,
+    IMGcontainer,
+};
